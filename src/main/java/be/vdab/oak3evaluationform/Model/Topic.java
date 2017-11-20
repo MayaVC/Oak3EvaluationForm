c5588168c592d078168dbcd30151b3a8e21c4753
Marco <m.kohnen@telenet.be>
2017-11-20 12:51:36 +0100

    Aanmaak klasses : Topic, Subtopic en Evaluation

--- src/main/java/be/vdab/oak3evaluationform/Model/Topic.java
+++ src/main/java/be/vdab/oak3evaluationform/Model/Topic.java
@@ -1,4 +1,44 @@
 package be.vdab.oak3evaluationform.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Topic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int id;
+    @NotNull
+    String name;
+
+    public Topic() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Topic{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
