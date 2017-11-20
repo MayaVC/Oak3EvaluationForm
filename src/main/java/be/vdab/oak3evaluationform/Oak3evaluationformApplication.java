251c3ca858816987d5b3b257e6dcda7eb8433bf0
Maya <mayavc@icloud.com>
2017-11-20 11:05:51 +0100

    Initial commit

---
+++ src/main/java/be/vdab/oak3evaluationform/Oak3evaluationformApplication.java
@@ -0,0 +1,12 @@
+package be.vdab.oak3evaluationform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oak3evaluationformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oak3evaluationformApplication.class, args);
+	}
+}
