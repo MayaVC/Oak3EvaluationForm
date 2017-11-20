98cb5c8d2959ad7bcaf931e30b13a1596a0d71c4
Maya <mayavc@icloud.com>
2017-11-20 12:55:13 +0100

    Classes Category, Question, EvaluationQuestions

--- src/main/java/be/vdab/oak3evaluationform/Model/EvaluationQuestion.java
+++ src/main/java/be/vdab/oak3evaluationform/Model/EvaluationQuestion.java
@@ -1,4 +1,64 @@
 package be.vdab.oak3evaluationform.Model;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotNull;
+
 public class EvaluationQuestion {
+
+    @NotNull
+    private int evaluationId;
+    @NotNull
+    private int categoryId;
+    @NotNull
+    private int questionId;
+    @NotNull
+    private EnumScore score;
+
+    public EvaluationQuestion(int evaluationId) {
+        this.evaluationId = evaluationId;
+    }
+
+    public int getEvaluationId() {
+        return evaluationId;
+    }
+
+    public void setEvaluationId(int evaluationId) {
+        this.evaluationId = evaluationId;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+    @Enumerated(EnumType.STRING)
+    public EnumScore getScore() {
+        return score;
+    }
+
+    public void setScore(EnumScore score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "EvaluationQuestion{" +
+                "evaluationId=" + evaluationId +
+                ", categoryId=" + categoryId +
+                ", questionId=" + questionId +
+                ", score=" + score +
+                '}';
+    }
 }
--- src/main/java/be/vdab/oak3evaluationform/Model/Question.java
+++ src/main/java/be/vdab/oak3evaluationform/Model/Question.java
@@ -1,4 +1,43 @@
 package be.vdab.oak3evaluationform.Model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
 public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @NotNull
+    private String name;
+
+    public Question(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
