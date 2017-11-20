4a9e32172df2d0d7efd541c1e52c0e0a74fef0ce
Miel Noelanders <mielnoelanders.qnh@outlook.com>
2017-11-20 12:44:49 +0100

    Courses aangemaakt

--- src/main/java/be/vdab/oak3evaluationform/Model/Instructor.java
+++ src/main/java/be/vdab/oak3evaluationform/Model/Instructor.java
@@ -1,4 +1,45 @@
 package be.vdab.oak3evaluationform.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @NotNull
+    private String name;
+
+    public Instructor() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Instructor{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- src/main/java/be/vdab/oak3evaluationform/Model/Student.java
+++ src/main/java/be/vdab/oak3evaluationform/Model/Student.java
@@ -1,4 +1,69 @@
 package be.vdab.oak3evaluationform.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String eMail;
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", eMail='" + eMail + '\'' +
+                '}';
+    }
 }
