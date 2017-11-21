2590be891ed5296d1e2796c465bcc3ba68caced5
Maya <mayavc@icloud.com>
2017-11-21 10:59:32 +0100

    Service Question and EvaluationQuestion

---
+++ src/main/java/be/vdab/oak3evaluationform/Service/QuestionService.java
@@ -0,0 +1,13 @@
+package be.vdab.oak3evaluationform.Service;
+
+import be.vdab.oak3evaluationform.Model.Question;
+import java.util.List;
+
+public interface QuestionService {
+
+    int addQuestion(Question question);
+    int updateQuestion(Question question);
+    int deleteQuestion(Question question);
+
+    List<Question> findAll();
+}
