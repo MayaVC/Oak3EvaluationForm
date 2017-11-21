0e49ebdb394d28bbb4be2a8bfb737495d2d0f0c8
Miel Noelanders <mielnoelanders.qnh@outlook.com>
2017-11-21 10:57:21 +0100

    InstructorStudentCourse Repositories met methods

--- src/main/java/be/vdab/oak3evaluationform/Service/CourseService.java
+++ src/main/java/be/vdab/oak3evaluationform/Service/CourseService.java
@@ -2,8 +2,16 @@ package be.vdab.oak3evaluationform.Service;
 
 import be.vdab.oak3evaluationform.Model.Course;
 
+import java.util.List;
+
 public interface CourseService {
 
     int addCourse(Course course);
+
+    int deleteCourse(Course course);
+
+    int updateCourse(Course course);
+
+    List<Course> findAll();
 
 }
